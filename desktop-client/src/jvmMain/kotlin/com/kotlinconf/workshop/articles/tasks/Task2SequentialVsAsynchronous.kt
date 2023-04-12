b633d013e8b601c2a5f236d5c1d6154a005a460e
Renato Costa <renato@closelink.net>
2023-04-12 09:42:50 +0200

    task2 - Block vs suspend implementation in the article loading

--- desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/articles/tasks/Task2SequentialVsAsynchronous.kt
+++ desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/articles/tasks/Task2SequentialVsAsynchronous.kt
@@ -3,10 +3,15 @@ package com.kotlinconf.workshop.articles.tasks
 import com.kotlinconf.workshop.articles.model.Article
 import com.kotlinconf.workshop.articles.network.BlogService
 import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 
 // Task: Implement concurrent loading of articles
 suspend fun loadArticlesConcurrently(service: BlogService): List<Article> = coroutineScope {
-    TODO()
+    service.getArticleInfoList().associateWith { async { service.getComments(articleInfo = it) } }
+        .map { (articleInfo, commentsDeferred) ->
+            Article(
+                info = articleInfo,
+                comments = commentsDeferred.await(),
+            )
+        }
 }
