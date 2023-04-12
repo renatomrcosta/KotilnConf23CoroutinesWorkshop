95174632ad7e8489fcc254dd9d038433a75af521
Renato Costa <renato@closelink.net>
2023-04-12 11:39:59 +0200

    task 3

--- desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/articles/tasks/Task3StructuralConcurrency.kt
+++ desktop-client/src/jvmMain/kotlin/com/kotlinconf/workshop/articles/tasks/Task3StructuralConcurrency.kt
@@ -4,9 +4,15 @@ import com.kotlinconf.workshop.articles.model.Article
 import com.kotlinconf.workshop.articles.network.BlogService
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
 
 // Run both versions and try to cancel
 suspend fun loadArticlesNonCancelable(service: BlogService): List<Article> {
-    TODO()
+    return service.getArticleInfoList()
+        .associateWith { GlobalScope.async { service.getComments(articleInfo = it) } }
+        .map { (articleInfo, commentsDeferred) ->
+            Article(
+                info = articleInfo,
+                comments = commentsDeferred.await(),
+            )
+        }
 }
